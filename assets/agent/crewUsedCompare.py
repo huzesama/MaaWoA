6c41aced153891aed29e1f706f3eb1cabe475ba1
venmzvi <1215186276@qq.com>
2026-09-12 00:37:08 +0800

    feat: 基本功能迭代更新

--- assets/agent/crewUsedCompare.py
+++ assets/agent/crewUsedCompare.py
@@ -3,8 +3,9 @@ from maa.custom_recognition import CustomRecognition
 from maa.context import Context
 import json
 
-# 用于保存上一次 OCR 结果的模块级变量
+# 模块级变量
 _last_ocr_crew_used: str = "???/???"
+_skip_compare: bool = False
 
 @AgentServer.custom_recognition("crewUsedCompare")
 class crewUsedCompare(CustomRecognition):
@@ -13,7 +14,8 @@ class crewUsedCompare(CustomRecognition):
 		context: Context,
 		argv: CustomRecognition.AnalyzeArg,
 	) -> CustomRecognition.AnalyzeResult:
-		global _last_ocr_crew_used
+		global _last_ocr_crew_used, _skip_compare
+			
 		try:
 			param = json.loads(argv.custom_recognition_param) if argv.custom_recognition_param else {}
 		except json.JSONDecodeError:
@@ -21,34 +23,39 @@ class crewUsedCompare(CustomRecognition):
 		if not isinstance(param, dict):  
 			param = {}  
 		update_cache = param.get("update_cache", True)
-
-		reco_detail = context.run_recognition(
-			"__crewUsedCompareOCR__",
-			argv.image,
-			pipeline_override={
-				"__crewUsedCompareOCR__": {
-					"recognition":"OCR",
-					"only_rec": True,
-					"roi": "cacheCrewUsedBox",
-					"replace": ["\\|","/"],
-				}
-			}
-		)
-
+		_return_box_=(0, 0, 0, 0)
 		current_ocr_crew_used = ""
-		if reco_detail and reco_detail.hit:
-			current_ocr_crew_used = getattr(reco_detail.best_result, "text", "")
 
-		# 对比本次与上一次 OCR 内容
-		if current_ocr_crew_used == _last_ocr_crew_used:
-			#context.override_next(argv.node_name, ["NodeA"])#相等则使recognition不命中
-			_return_box_=None
-		else:
-			#context.override_next(argv.node_name, ["NodeB"])#不相等则使recognition命中
-			_return_box_=reco_detail.box if reco_detail and reco_detail.hit else (0, 0, 0, 0)
+		try:			
+			if not _skip_compare:
+				reco_detail = context.run_recognition(
+					"__crewUsedCompareOCR__",
+					argv.image,
+					pipeline_override={
+						"__crewUsedCompareOCR__": {
+							"recognition":"OCR",
+							#"only_rec": True,
+							"roi": "cacheCrewUsedBox",
+							"replace": ["\\|","/"],
+						}
+					}
+				)
+
+				if reco_detail and reco_detail.hit:
+					current_ocr_crew_used = getattr(reco_detail.best_result, "text", "?/?")
+
+				# 对比本次与上一次 OCR 内容
+				if current_ocr_crew_used == _last_ocr_crew_used:
+					_return_box_=None
+				else:
+					_skip_compare = True
+					_return_box_=reco_detail.box if reco_detail and reco_detail.hit else (0, 0, 0, 0)
+		except Exception as e:
+			print(f"[crewUsedCompared] analyze error: {e}")
 
 		# 仅当 update_cache 为 True 时才更新缓存
 		if update_cache:
+			_skip_compare = False
 			if not current_ocr_crew_used:
 				print(f'[crewUsedCompared]: handling busy reco. Last cache Crew: {_last_ocr_crew_used}  update cache Crew: reco faild.')
 				_last_ocr_crew_used = "???/???"
